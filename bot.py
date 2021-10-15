54f7dfa1ec116e2e52e40fa6bb841250798f7913
ASparks1 <sparksmkii@gmail.com>
2021-10-15 11:06:30 +0200

    Implementing some best practices

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-import discord
 import os
-import dotenv
 import sqlite3
 from datetime import datetime
+import discord
+import dotenv
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
 from discord_components import DiscordComponents, Button, ButtonStyle
@@ -22,8 +22,8 @@ from Helpers import MemberHelper
 from Scripts import MakeDatabase
 from Scripts import InsertMasterData
 
-# Enable privileged gateway intents as described on 
-# https://discordpy.readthedocs.io/en/latest/intents.html 
+# Enable privileged gateway intents as described on
+# https://discordpy.readthedocs.io/en/latest/intents.html
 # so we can access member objects to retrieve display names and use reactions
 intents = discord.Intents.default()
 intents.members = True
@@ -53,59 +53,50 @@ async def on_member_remove(member):
 @bot.command(name='templates', aliases=['Templates'])
 async def templates(ctx):
   await Templates.GetTemplates(ctx.message, bot)
-  
+
 @bot.command(name='runs', aliases=['Runs'])
 async def runs(ctx):
   await Runs.ListRunsOnDate(ctx.message, bot)
-  
+
 @bot.command(name='commands', aliases=['Commands'])
 async def commands(ctx):
   await Commands.ListCommands(ctx.message, bot)
-  
+
 @bot.command(name='roles', aliases=['Roles'])
 async def roles(ctx):
   await Roles.ListRoles(ctx.message)
-  
+
 @bot.command(name='addrun', aliases=['Addrun'])
 async def addrun(ctx):
   await AddRun.AddRunInDM(ctx.message, bot)
-  
+
 @bot.command(name='adddefaulttemplates', aliases=['Adddefaulttemplates','AddDefaultTemplates'])
 async def adddefaulttemplates(ctx):
   await AddDefaultTemplates.AddDefaultTemplates(ctx.message)
-  
+
 @bot.command(name='addtemplate', aliases=['Addtemplate','AddTemplate'])
 async def addtemplate(ctx):
   await AddTemplate.AddTemplate(ctx.message, bot)
-  
+
 @bot.command(name='deletetemplate', aliases=['Deletetemplate','DeleteTemplate'])
 async def deletetemplate(ctx):
   await DeleteTemplate.DeleteTemplate(ctx.message, bot)
-  
+
 @bot.command(name='dismiss', aliases=['Dismiss'])
 async def dismiss(ctx):
   await Dismiss.DismissMember(ctx.message, bot)
-  
+
 @bot.command(name='myruns', aliases=['Myruns','MyRuns'])
 async def myruns(ctx):
   await MyRuns.ListMyRuns(ctx.message, bot)
-  
+
 @bot.command(name='makedatabase', aliases=['Makedatabase','MakeDatabase'])
 async def makedatabase(ctx):
   await MakeDatabase.MakeDatabase(ctx.message)
-  
+
 @bot.command(name='insertmasterdata', aliases=['Insertmasterdata','InsertMasterdata'])
 async def insertmasterdata(ctx):
   await InsertMasterData.InsertMasterData(ctx.message)
-  
-@bot.command(name='inserttestdata', aliases=['Inserttestdata','InsertTestdata'])
-async def inserttestdata(ctx):
-  conn = sqlite3.connect('RaidPlanner.db')
-  c = conn.cursor()
-  
-  c.execute(f"DELETE FROM RaidReserves WHERE ID = 5")
-  conn.commit()
-  conn.close()
 
 # Message events
 # Do nothing if the message is from the bot
@@ -113,7 +104,7 @@ async def inserttestdata(ctx):
 async def on_message(message):
   if message.author == bot.user:
     return
-  # Process commmands if found in message 
+  # Process commmands if found in message
   await bot.process_commands(message)
 
 # Button events
