b4f2a58f497a272537c985d6d9435261daac7efc
ASparks1 <sparksmkii@gmail.com>
2021-11-17 11:07:17 +0100

    Improve readability and maintainability

--- Helpers/ButtonInteractionHelper.py
+++ Helpers/ButtonInteractionHelper.py
@@ -1,7 +1,5 @@
 from Helpers import DMHelper
-from Helpers import ReactionHelper
-from Helpers import MessageHelper
-from Commands import Join
+from Helpers import ButtonRowHelper
 
 async def OnButtonClick(interaction, bot):
   try:
@@ -10,79 +8,9 @@ async def OnButtonClick(interaction, bot):
     await DMHelper.DMUserByID(bot, UserID, "Something went wrong resolving server information.")
     return
 
-  if interaction.custom_id == "tank_btn":
-    try:
-      await interaction.respond(type=6)
-      await Join.JoinRaid(interaction.message, bot, "tank", UserID)
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "dps_btn":
-    try:
-      await interaction.respond(type=6)
-      await Join.JoinRaid(interaction.message, bot, "dps", UserID)
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "healer_btn":
-    try:
-      await interaction.respond(type=6)
-      await Join.JoinRaid(interaction.message, bot, "healer", UserID)
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "rally_btn":
-    try:
-      await interaction.respond(type=6)
-      await ReactionHelper.OnAddRallyReaction(interaction.message, bot, UserID)
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "members_btn":
-    try:
-      Message = await ReactionHelper.OnMemberReaction(interaction.message, bot)
-      if Message:
-        await interaction.respond(type=4, content=f"{Message}")
-      elif not Message:
-        await interaction.respond(content="No message was returned")
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "reserves_btn":
-    try:
-      Message = await ReactionHelper.OnReservesReaction(interaction.message, bot)
-      if Message:
-        await interaction.respond(type=4, content=f"{Message}")
-      elif not Message:
-        await interaction.respond(type=4, content="No reserves have signed up for this run")
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "reschedule_btn":
-    try:
-      await interaction.respond(type=6)
-      await ReactionHelper.OnAddRescheduleReaction(interaction.message, bot, UserID)
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "cancel_btn":
-    try:
-      await interaction.respond(type=6)
-      await ReactionHelper.OnAddCancelReaction(interaction.message, bot, UserID)
-    except:
-     await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-     return
-  if interaction.custom_id == "editdesc_btn":
-    try:
-      await interaction.respond(type=6)
-      await ReactionHelper.OnAddEditDescReaction(interaction.message, bot, UserID)
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
-  if interaction.custom_id == "messageraidmembers_btn":
-    try:
-      await interaction.respond(type=6)
-      await MessageHelper.MessageRaidMembers(interaction.message, bot, UserID)
-    except:
-      await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining information for this run.")
-      return
+  if interaction.custom_id in ("tank_btn","dps_btn","healer_btn","rally_btn"):
+    await ButtonRowHelper.FirstRowButtons(interaction, bot, UserID)
+  if interaction.custom_id in ("members_btn","reserves_btn","messageraidmembers_btn"):
+    await ButtonRowHelper.SecondRowButtons(interaction, bot, UserID)
+  if interaction.custom_id in ("editdesc_btn","reschedule_btn","cancel_btn"):
+    await ButtonRowHelper.ThirdRowButtons(interaction, bot, UserID)
