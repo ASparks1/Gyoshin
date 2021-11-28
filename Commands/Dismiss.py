da995bc5d619629c13d5703258356e4fab256337
ASparks1 <sparksmkii@gmail.com>
2021-11-28 16:45:57 +0100

    Moved dismiss functionality to button

--- Commands/Dismiss.py
+++ Commands/Dismiss.py
@@ -1,92 +1,103 @@
+import asyncio
+import discord
 import sqlite3
+from discord import ChannelType
 from Helpers import DateTimeFormatHelper
-from Helpers import OriginHelper
-from Helpers import UserHelper
-from Helpers import RoleHelper
 from Helpers import DMHelper
+from Helpers import DismissHelper
+from Helpers import MemberHelper
+from Helpers import MessageHelper
+from Helpers import RaidIDHelper
+from Helpers import UserHelper
 
-async def DismissMember(message, client):
-  commands = message.content.split()
+async def DismissMembers(bot, message, UserID):
+  def DMCheck(dm_message):
+    return dm_message.channel.type == ChannelType.private and dm_message.author.id == UserID
+
   try:
-    RaidID = commands[1]
-    UserID = message.mentions[0].id
+    RaidID = await RaidIDHelper.GetRaidIDFromMessage(message)
   except:
-    await DMHelper.DMUser(message, "Something went wrong processing the run number or user to dismiss")
+    await DMHelper.DMUserByID(bot, UserID, "Something went wrong obtaining run information")
     return
 
-  UserName = await UserHelper.GetDisplayName(message, UserID, client)
-  Creator = message.author.id
-
-  if not Creator:
-    await DMHelper.DMUser(message, "Something went wrong retrieving the user ID")
-    # Delete message that contains command
-    await message.delete()
+  try:
+    conn = sqlite3.connect('RaidPlanner.db')
+    c = conn.cursor()
+    c.execute("SELECT OrganizerUserID FROM Raids WHERE ID = (?)",(RaidID,))
+    row = c.fetchone()
+    OrganizerUserID = row[0]
+    if OrganizerUserID == UserID:
+      c.execute("SELECT RM.UserID FROM RaidMembers RM JOIN Raids R on R.ID = RM.RaidID WHERE R.ID = (?) AND R.OrganizerUserID = (?) AND RM.UserID != (?)", (RaidID, UserID, UserID))
+      RaidMembers = c.fetchall()
+      c.execute("SELECT Name, Date FROM Raids WHERE ID = (?)", (RaidID,))
+      row = c.fetchone()
+      RaidName = row[0]
+      Date = row[1]
+      LocalDate = await DateTimeFormatHelper.SqliteToLocalNoCheck(Date)
+      conn.close()
+      # Converting tuple result into list for later use
+      RaidMembers = [int(x) for x, in RaidMembers]
+      await MemberHelper.ListMembersForOrganizer(message, bot, UserID)
+    else:
+      conn.close()
+      await DMHelper.DMUserByID(bot, UserID, "Only the organizer of this run is allowed to dismiss members.")
+      return
+  except:
+    conn.close()
+    await DMHelper.DMUserByID(bot, UserID, "Something went wrong retrieving raid members")
     return
 
-  if UserID == Creator:
-        await DMHelper.DMUser(message, "You cannot dismiss yourself as the organizer of this run")
-        await message.delete()
+  if RaidMembers:
+    MembersToDismiss = None
+    while not MembersToDismiss:
+      await DMHelper.DMUserByID(bot, UserID, "Please enter the number(s) of the member(s) you want to dismiss separated by spaces if you want to dismiss more then 1 member.")
+      try:
+        response = await bot.wait_for(event='message', timeout=60, check=DMCheck)
+      except asyncio.TimeoutError:
+        await DMHelper.DMUserByID(bot, UserID, "Your request has timed out, please click the button again from the channel if you still want to dismiss members from this run.")
         return
 
-  await message.delete()
+      try:
+        MembersToDismiss = response.content
+        MembersToDismiss = str.split(MembersToDismiss, ' ')
+        MembersToDismissMessage = None
+        for MemberToDismiss in MembersToDismiss:
+          MemberToDismissNumber = int(MemberToDismiss[0])
+          MemberToDismissIndex = MemberToDismissNumber - 1
+          MemberToDismiss = RaidMembers[MemberToDismissIndex]
+          MemberToDismissDisplayName = await UserHelper.GetDisplayName(message, MemberToDismiss, bot)
+          if MembersToDismissMessage:
+            MembersToDismissMessage = f"{MembersToDismissMessage} and {MemberToDismissDisplayName}"
+            DismissMessage = "have been dismissed"
+          if not MembersToDismissMessage:
+            MembersToDismissMessage = MemberToDismissDisplayName
+            DismissMessage = "has been dismissed"
+      except:
+        await DMHelper.DMUserByID(bot, UserID, "Please enter valid number(s).")
+        continue
 
-  conn = sqlite3.connect('RaidPlanner.db')
-  c = conn.cursor()
-
-  try:
-    c.execute("SELECT ID, Name, Date FROM Raids WHERE ID = (?)", (RaidID,))
-    row = c.fetchone()
-    RaidID = row[0]
-    RaidName = row[1]
-    Date = row[2]
-    Date = await DateTimeFormatHelper.SqliteToLocal(message, Date)
-  except:
-    await DMHelper.DMUser(message, f"Run {RaidID} not found or you are not the organizer of this run, only the organizer is allowed to dismiss members.")
-    conn.close()
-    return
-
-  if RaidID:
-    try:
-      c.execute("SELECT ID, RoleID FROM RaidMembers WHERE RaidID = (?) AND UserID = (?)", (RaidID, UserID,))
-      row = c.fetchone()
-      RaidMemberID = row[0]
-      RoleID = row[1]
-      RoleName = await RoleHelper.GetRoleName(RoleID)
-    except:
-      await DMHelper.DMUser(message, "Something went wrong checking if the provided member is part of this run")
-      conn.close()
-      return
-  else:
-    conn.close()
-    return
-
-  if RaidMemberID:
-    try:
-      c.execute("DELETE FROM RaidMembers WHERE ID = (?)", (RaidMemberID,))
-    except:
-      await DMHelper.DMUser(message, "Something went wrong removing this member from the run")
-      conn.close()
-      return
-
-    try:
-      if RoleName == 'tank':
-        c.execute("Update Raids SET NrOfPlayersSignedUp = NrOfPlayersSignedUp - 1, NrOfTanksSignedUp = NrOfTanksSignedUp - 1, Status = 'Forming' WHERE ID = (?)", (RaidID,))
-      elif RoleName == 'dps':
-        c.execute("Update Raids SET NrOfPlayersSignedUp = NrOfPlayersSignedUp - 1, NrOfDpsSignedUp = NrOfDpsSignedUp - 1, Status = 'Forming' WHERE ID = (?)", (RaidID,))
-      elif RoleName == 'healer':
-        c.execute("Update Raids SET NrOfPlayersSignedUp = NrOfPlayersSignedUp - 1, NrOfHealersSignedUp = NrOfHealersSignedUp - 1, Status = 'Forming' WHERE ID = (?)", (RaidID,))
-    except:
-      await DMHelper.DMUser(message, "Something went wrong updating the number of signed up players")
-      conn.close()
-      return
-    try:
-      conn.commit()
-      await message.channel.send(f"{UserName} has been dismissed from the run {RaidName} on {Date}")
-      conn.close()
-    except:
-      conn.close()
-      await DMHelper.DMUser(message, "Something went wrong dismissing a member from this run")
-      return
-  else:
-    conn.close()
-    return
+    CanDismiss = None
+    while not CanDismiss:
+      try:
+        await DMHelper.DMUserByID(bot, UserID, f"Do you want to dismiss {MembersToDismissMessage} from {RaidName} on {LocalDate}? (Y/N)")
+        response = await bot.wait_for(event='message', timeout=60, check=DMCheck)
+        if response.content in("Y","y","Yes","yes"):
+          CanDismiss = 'yes'
+          for MemberToDismiss in MembersToDismiss:
+            MemberToDismissNumber = int(MemberToDismiss[0])
+            MemberToDismissIndex = MemberToDismissNumber - 1
+            MemberToDismiss = RaidMembers[MemberToDismissIndex]
+            await DismissHelper.DismissMember(bot, RaidID, MemberToDismiss, UserID)
+          UpdatedMessage = await MessageHelper.UpdateRaidInfoMessage(message, bot, UserID)
+          if UpdatedMessage:
+            await message.edit(content=UpdatedMessage)
+          await message.channel.send(f"{MembersToDismissMessage} {DismissMessage} from {RaidName} on {LocalDate}.")
+        elif response.content in("N","n","No","no"):
+          CanDismiss = "no"
+          return
+        else:
+          await DMHelper.DMUserByID(bot, UserID, "Invalid answer detected, please respond with yes or no.")
+          continue
+      except asyncio.TimeoutError:
+        await DMHelper.DMUserByID(bot, UserID, "Your request has timed out, please click the button again from the channel if you still want to dismiss members for this run.")
+        return
