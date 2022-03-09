401de469103414a43d34723f95cab4315048aae1
ASparks1 <sparksmkii@gmail.com>
2022-03-09 11:18:13 +0100

    Changed userid to lowercase

--- Helpers/UserHelper.py
+++ Helpers/UserHelper.py
@@ -6,23 +6,23 @@ async def GetDisplayName(ctx, userid, bot):
   Origin = await OriginHelper.GetOrigin(ctx, bot, userid)
   guild = bot.get_guild(Origin)
   if not Origin:
-    await DMHelper.DMUserByID(bot, UserID, "Something went wrong retrieving the server ID")
+    await DMHelper.DMUserByID(bot, userid, "Something went wrong retrieving the server ID")
     return
 
   try:
     member_obj = await guild.fetch_member(userid)
   except:
-    await DMHelper.DMUserByID(bot, UserID, "Something went wrong retrieving this users display name, perhaps they have left the server")
+    await DMHelper.DMUserByID(bot, userid, "Something went wrong retrieving this users display name, perhaps they have left the server")
   try:
     if member_obj:
       display_name = member_obj.display_name
       return display_name
   except:
-    await DMHelper.DMUserByID(bot, UserID, "Something went wrong retrieving the users' display name")
+    await DMHelper.DMUserByID(bot, userid, "Something went wrong retrieving the users' display name")
     return
 
   if not display_name:
-    await DMHelper.DMUserByID(bot, UserID, "Something went wrong retrieving the users' display name")
+    await DMHelper.DMUserByID(bot, userid, "Something went wrong retrieving the users' display name")
     return
   return display_name
 
@@ -30,6 +30,6 @@ async def GetUserID(ctx, userid, bot):
   userid = ctx.author.id
 
   if not userid:
-    await DMHelper.DMUserByID(bot, UserID, "Something went wrong retrieving the user id")
+    await DMHelper.DMUserByID(bot, userid, "Something went wrong retrieving the user id")
     return
   return userid
